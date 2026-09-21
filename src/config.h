2f01acbd6d99ad6406647c1ecb417c166bf08a1a
Program <67320703+Program132@users.noreply.github.com>
2026-09-21 14:35:08 +0200

    Project Config

---
+++ src/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define DEFAULT_DATABASE_DIR "database"
+#define DEFAULT_DATASETS_DIR "datasets"
+
+#define TOKEN_START_SENTENCE "<BOS>"
+#define TOKEN_END_SENTENCE "<EOS>"
+#define TOKEN_UNKNOWN_WORD "<UNK>"
+
+#define REGEX_WORD "\b\w+\b|[^\w\s]"
+
+#endif
